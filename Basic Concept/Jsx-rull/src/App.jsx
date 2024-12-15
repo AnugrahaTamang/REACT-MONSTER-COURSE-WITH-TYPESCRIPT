878e9a36ea5a5bf761f588940de24766bd86b97a
AnugrahaTamang <tamanganugrah59@gmail.com>
2024-12-15 18:08:37 +0545

    jsx rull

---
+++ Basic Concept/Jsx-rull/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1>jsx rules</h1>
+      <p>jsx must return a single parent element</p>
+      <p>jsx element must be properly closed</p>
+      <p>jsx attribute are written using camecase (example: className instead of class, htmlfor instead of for)</p>
+    </div>
+  )
+}
+
+export default App
